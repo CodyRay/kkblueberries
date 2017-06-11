84f059f1230c1ec4fd55f236097d75450a3e582f
Cody Ray Freeman Hoeft <crhoeft@gmail.com>
2017-06-11 16:58:32 -0700

    Create post_optimization.js

---
+++ post_optimization.js
@@ -0,0 +1,54 @@
+/* 
+ * git status
+ * npm run eject
+ * modify webpack production to not inline images
+ * add 'babel-node post_optimization.js to predeploy
+ */
+
+const critical = require('critical');
+const path = require('path');
+const fs = require('fs');
+const glob = require('glob-promise')
+const _ = require('lodash')
+const process = require('process');
+
+fs.mkdirSync('optimized')
+fs.renameSync('./build', './optimized/kkblueberries')
+process.chdir('./optimized')
+
+glob('./**/*.html')
+  .then((html_files) => {
+    console.log(html_files);
+    return _.reduce(html_files, ((promise, html) =>
+      promise
+        .then(() =>
+          critical.generate({
+            inline: true,
+            base: process.cwd(),
+            src: path.resolve(html),
+            dest: path.resolve(path.dirname(html), path.basename(html, '.html') + '.critical.html'),
+            minify: true,
+            inlineImages: false,
+          })
+        )
+        .catch((ex) => {
+          console.log(ex);
+        })
+    ), Promise.resolve())
+  })
+  .then(() =>
+    glob('./**/*.critical.html')
+  )
+  .then((html_files) => {
+    _.forEach(html_files, (file) => {
+      var original = path.resolve(path.dirname(file), path.basename(file, '.critical.html') + '.html')
+      fs.unlinkSync(original)
+      fs.renameSync(file, original)
+    });
+    process.chdir('..')
+    fs.renameSync('./optimized/kkblueberries', './build')
+    // fs.unlinkSync('optimized')
+  })
+  .catch((ex) => {
+    console.log(ex);
+  })
