b4faa2a20b881ac6324633c6e4f514bdb053aca2
Cody Ray Hoeft <crhoeft@gmail.com>
2019-06-21 14:59:11 -0600

    Switch to open season message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import React from 'react';
-import Helmet from 'react-helmet';
-import Modal from 'react-modal';
-import { Route, Switch, Redirect } from 'react-router';
-import Body from './Body.js'
-import Home from './Home';
-import Photos from './Photos';
-import Information from './Information';
+import React from "react";
+import Helmet from "react-helmet";
+import Modal from "react-modal";
+import { Route, Switch, Redirect } from "react-router";
+import Body from "./Body.js";
+import Home from "./Home";
+import Photos from "./Photos";
+import Information from "./Information";
 
-// Title 
-const baseTitle = 'K&K  Blueberries - U-Pick Blueberries in Hermiston, Oregon!';
-const defaultTitle = 'Welcome to ' + baseTitle;
-const titleTemplate = '%s ' + baseTitle;
+// Title
+const baseTitle = "K&K  Blueberries - U-Pick Blueberries in Hermiston, Oregon!";
+const defaultTitle = "Welcome to " + baseTitle;
+const titleTemplate = "%s " + baseTitle;
 
 // Meta tags
-const metaDescription = 'K & K Blueberries offers u-pick blueberries in Hermiston, Oregon, located in the pacific northwest';
-const metaKeywords = 'blueberries, pick bluberries, u-pick, family blueberry farm, Hermiston bluberries, fun u-pick farms, berry farm pacific northwest, July blueberries, bluberry farm, Umatilla county farm, Minnehaha farm';
+const metaDescription =
+  "K & K Blueberries offers u-pick blueberries in Hermiston, Oregon, located in the pacific northwest";
+const metaKeywords =
+  "blueberries, pick bluberries, u-pick, family blueberry farm, Hermiston bluberries, fun u-pick farms, berry farm pacific northwest, July blueberries, bluberry farm, Umatilla county farm, Minnehaha farm";
 
 // Status message shown at the top of all pages
-const statusMessage = <p>We are now <strong>closed for the 2018 season</strong>. Thank you for coming out and supporting our small farm. See you next year! Call or visit our <a href="https://www.facebook.com/K-and-K-Blueberries-104303779663777/" target="_blank" rel="noopener noreferrer">Facebook page</a> for up to date information.</p>
+const statusMessage = (
+  <p>
+    We are opening for the 2019 fresh blueberry picking season on{" "}
+    <strong>Monday, June 24th</strong>! Looking forward to seeing our returning
+    customers and meeting our new ones! Call or visit our{" "}
+    <a
+      href="https://www.facebook.com/K-and-K-Blueberries-104303779663777/"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Facebook page
+    </a>{" "}
+    for up to date information.
+  </p>
+);
 
-Modal.setAppElement('#app');
+// const statusMessage = (
+//   <p>
+//     We are now <strong>closed for the 2018 season</strong>. Thank you for coming
+//     out and supporting our small farm. See you next year! Call or visit our{" "}
+//     <a
+//       href="https://www.facebook.com/K-and-K-Blueberries-104303779663777/"
+//       target="_blank"
+//       rel="noopener noreferrer"
+//     >
+//       Facebook page
+//     </a>{" "}
+//     for up to date information.
+//   </p>
+// );
+
+Modal.setAppElement("#app");
 
 export default () => (
   <div id="app">
@@ -29,11 +60,11 @@ export default () => (
     </Helmet>
     <Body statusMessage={statusMessage}>
       <Switch>
-        <Route exact path='/' component={Home} />
+        <Route exact path="/" component={Home} />
         <Route exact path="/photos" component={Photos} />
         <Route exact path="/info" component={Information} />
-        <Redirect to='/' />
+        <Redirect to="/" />
       </Switch>
     </Body>
-  </div >
+  </div>
 );
