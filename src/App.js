ce9edb92a85e9f280bbb5f3f69c5ec60375e9fa6
Cody Ray Hoeft <cody.ray.hoeft@seeq.com>
2019-07-15 07:54:25 -0600

    Switch to closed message

--- src/App.js
+++ src/App.js
@@ -19,26 +19,11 @@ const metaKeywords =
   "blueberries, pick bluberries, u-pick, family blueberry farm, Hermiston bluberries, fun u-pick farms, berry farm pacific northwest, July blueberries, bluberry farm, Umatilla county farm, Minnehaha farm";
 
 // Status message shown at the top of all pages
-const statusMessage = (
-  <p>
-    We are opening for the 2019 fresh blueberry picking season on{" "}
-    <strong>Monday, June 24th</strong>! Looking forward to seeing our returning
-    customers and meeting our new ones! Call or visit our{" "}
-    <a
-      href="https://www.facebook.com/K-and-K-Blueberries-104303779663777/"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      Facebook page
-    </a>{" "}
-    for up to date information.
-  </p>
-);
-
 // const statusMessage = (
 //   <p>
-//     We are now <strong>closed for the 2018 season</strong>. Thank you for coming
-//     out and supporting our small farm. See you next year! Call or visit our{" "}
+//     We are opening for the 2019 fresh blueberry picking season on{" "}
+//     <strong>Monday, June 24th</strong>! Looking forward to seeing our returning
+//     customers and meeting our new ones! Call or visit our{" "}
 //     <a
 //       href="https://www.facebook.com/K-and-K-Blueberries-104303779663777/"
 //       target="_blank"
@@ -49,6 +34,23 @@ const statusMessage = (
 //     for up to date information.
 //   </p>
 // );
+
+const statusMessage = (
+  <p>
+    K {'&'} K Blueberries has <strong>closed for the 2019 season</strong>.
+    Thank you to all my amazing customers, both new and faithful, it was a
+    wonderful year for picking sweet blueberries. Remember to start looking again
+    next year around Father's Day! Call or visit our{" "}
+    <a
+      href="https://www.facebook.com/K-and-K-Blueberries-104303779663777/"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Facebook page
+    </a>{" "}
+    for up to date information.
+  </p>
+);
 
 Modal.setAppElement("#app");
 
